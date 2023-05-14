7405b67b130c165fc8f0cdf4520fd4de11969a62
asdfkco <84617097+asdfkco@users.noreply.github.com>
2023-05-15 08:23:13 +0900

    board

---
+++ board_ex_re/react-board/src/index.tsx
@@ -0,0 +1,8 @@
+import './index.css'
+import App from './App'
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
---
+++ board_ex_re/server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors')
+const port = 8000;
+
+app.use(cors({credentials:true,origin:"http://localhost:3000"}))
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:'root',
+    password:'1234',
+    database:'bbs'
+})
+
+
+
+app.get('/', (req,res) =>{
+    const sqlQuery = "insert into requested (rowno) values(1)"
+    db.query(sqlQuery,(err,result)=>{
+        console.log(err)
+        res.send("success")
+    })
+    
+    console.log('reqeusted');
+});
+
+app.listen(port,()=>{
+    console.log(`port start ${port}`);
+})
